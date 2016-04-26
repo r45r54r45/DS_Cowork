e518ac353839d128c27516a9495fe223c8fdad75
Woohyun Kim <r45r54r45@naver.com>
2016-04-26 14:36:35 +0900

    BST implement

--- src/BST.java
+++ src/BST.java
@@ -17,33 +17,73 @@ public class BST <Key extends Comparable<Key>, Value> implements CommonMethod<Ke
 	public boolean contains(Key k){return get(k)!=null;}
 	@Override
 	public Value get(Key k) {
-		// TODO Auto-generated method stub
-		return null;
+		return get(root,k);
+	}
+	private Value get(Node n, Key k){
+		if(n==null)return null;
+		int t=n.getKey().compareTo(k);
+		if(t>0)return get(n.getLeft(),k);
+		else if(t<0)return get(n.getRight(),k);
+		else return (Value)n.getValue();
 	}
 	@Override
 	public void put(Key k, Value v) {
 		// TODO Auto-generated method stub
-		
+		root=put(root,k,v);
+	}
+	private Node put(Node n, Key k, Value v){
+		if(n==null)return new Node(k,v,1);
+		int t=n.getKey().compareTo(k);
+		if(t>0)n.setLeft(put(n.getLeft(),k,v));
+		else if(t<0)n.setRight(put(n.getRight(),k,v));
+		else n.setName(v);
+		n.setSubTreeSize(1+size(n.getLeft())+size(n.getRight()));
+		return n;
 	}
 	@Override
 	public Key min() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())return null;
+		return (Key)min(root).getKey();
+	}
+	private Node min(Node n){
+		if(n.getLeft()==null)return n;
+		return min(n.getLeft());
 	}
 	@Override
 	public void deleteMin() {
-		// TODO Auto-generated method stub
-		
+		if(isEmpty())throw new Error();
+		root=deleteMin(root);
+	}
+	private Node deleteMin(Node n){
+		if(n.getLeft()==null)return n.getRight();
+		n.setLeft(deleteMin(n.getLeft()));
+		n.setSubTreeSize(1+size(n.getLeft())+size(n.getRight()));
+		return n;
 	}
 	@Override
 	public void delete(Key k) {
-		// TODO Auto-generated method stub
-		
+		root=delete(root,k);
+	}
+	private Node delete(Node n, Key k){
+		if(n==null)return null;
+		int t=n.getKey().compareTo(k);
+		if(t>0)n.setLeft(delete(n.getLeft(),k));
+		else if(t<0)n.setRight(delete(n.getRight(),k));
+		else{
+			if(n.getRight()==null)return n.getLeft();
+			if(n.getLeft()==null)return n.getRight();
+			Node target=n;
+			n=min(target.getRight());
+			n.setRight(deleteMin(target.getRight()));
+			n.setLeft(target.getLeft());
+		}
+		n.setSubTreeSize(1+size(n.getLeft())+size(n.getRight()));
+		return n;
 	}
 	@Override
 	public String printTree() {
 		// TODO Auto-generated method stub
-		return null;
+		return "print tree";
 	}
 	
 	
