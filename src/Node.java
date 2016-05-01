15c48ce4ec36d7bebcdfd024bc499df148a95530
jis3613 <jis3613@gmail.com>
2016-05-02 04:38:26 +0900

    parent node field추가 node의 balance나타내는 필드 추가 getBalance getParent setParent 메서드 추가
    setLeft자리 이동
    getRight 코드 한줄로 바꿈

--- src/Node.java
+++ src/Node.java
@@ -2,8 +2,14 @@
 public class Node <Key extends Comparable<Key>, Value>{
 	private Key id;
 	private Value name;
+	public int balance;
 	private int subTreeSize;
-	private Node<Key, Value> left, right;
+	private Node<Key, Value> left, right, parent;
+	public Node(Key k) {
+		  left = right = parent = null;
+		  balance = 0;
+		  this.id= k;
+	}
 	public Node(Key newId, Value newName, int newSubTreeSize){
 		this.id=newId;
 		this.name=newName;
@@ -13,13 +19,20 @@ public class Node <Key extends Comparable<Key>, Value>{
 	} 
 	public Key getKey(){return id;}
 	public Value getValue(){return name;}
+	public int getBalance(){return balance;}
 	public int getSubTreeSize(){return subTreeSize;}
 	public Node<Key, Value> getLeft(){return left;}
-	public Node<Key, Value> getRight() {
-		return right;
+	public Node<Key, Value> getRight() {return right;}
+	public Node<Key, Value> getParent() {return parent;}
+	
+	public void setLeft(Node<Key, Value> newLeft) {
+		this.left = newLeft;
 	}
 	public void setRight(Node<Key, Value> newRight) {
 		this.right = newRight;
+	}
+	public void setParent(Node<Key, Value> newParent) {
+		this.parent = newParent;
 	}
 	public void setId(Key newId) {
 		this.id = newId;
@@ -30,8 +43,4 @@ public class Node <Key extends Comparable<Key>, Value>{
 	public void setSubTreeSize(int newSubTreeSize) {
 		this.subTreeSize = newSubTreeSize;
 	}
-	public void setLeft(Node<Key, Value> newLeft) {
-		this.left = newLeft;
-	}
-
 }
