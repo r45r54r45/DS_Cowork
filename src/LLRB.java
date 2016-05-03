5a56631d35d8c18123510ccb9531fa965cc977ed
yunjiljjh <yunjiljjh@naver.com>
2016-05-03 11:06:55 +0900

    LLRB 함수구현
    ㅈㄱㄴ

--- src/LLRB.java
+++ src/LLRB.java
@@ -10,6 +10,7 @@ public class LLRB <Key extends Comparable<Key>, Value> implements CommonMethod<K
 		if(n==null)return 0;
 		else return n.getSubTreeSize();
 	}
+	public boolean isRed(Node n){ return n.getColor();}
 	
 	public LLRB(){
 		root=null;
@@ -17,58 +18,145 @@ public class LLRB <Key extends Comparable<Key>, Value> implements CommonMethod<K
 	public LLRB(Key k,Value v){
 		root=new Node<Key, Value>(k,v,1);
 	}
-	 
-	@Override
+	
 	public Value get(Key k) {
-		// TODO Auto-generated method stub
-		 Node x = root;
-		 while (x != null){
-			 int cmp = k.compareTo((Key)x.getKey());
-			 if (cmp == 0) return (Value)x.getValue();
-			 else if (cmp < 0) x = x.getLeft(); 
-			 else if (cmp > 0) x = x.getRight();
-		 }
-		return null;
+		return get(root,k);
+	}
+	private Value get(Node n, Key k){
+		if(n==null)return null;
+		int t=n.getKey().compareTo(k);
+		if(t>0)return get(n.getLeft(),k);
+		else if(t<0)return get(n.getRight(),k);
+		else return (Value)n.getValue();
 	}
 
 	@Override
 	public void put(Key k, Value v) {
 		// TODO Auto-generated method stub
 		root = put((Node)root, (Key)k, (Value)v);
-		root.color = BLACK;
+		root.setColor(BLACK);
 	} 
 	
-	public void put(Node h, Key k, Value v) {
+	public Node put(Node h, Key k, Value v) {
 		// TODO Auto-generated method stub
-		   if (h == null) return new Node(key, value);
-	       if (isRed(h.getLeft()) && isRed(h.right)) colorFlip(h);
-	       int cmp = key.compareTo(h.key);       if (cmp == 0)     h.val = value;       else if (cmp < 0) h.left =  insert(h.left, key, value);       else              h.right = insert(h.right, key, value);
-	       if (isRed(h.right) && !isRed(h.left))    h = rotateLeft(h);       if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
-	       return h;   } 
-	} 
+		   if (h == null) return (new Node<Key, Value>(k, v));
+	       if (isRed(h.getLeft()) && isRed(h.getRight())) colorFlip(h);
+	       int cmp = k.compareTo((Key)h.getKey());
+	       if (cmp == 0) h.setName(v); 
+	       else if (cmp < 0) h.setLeft(put(h.getLeft(), k, v));
+	       else  h.setRight(put(h.getRight(), k, v));
+	       if (isRed(h.getRight()) && !isRed(h.getLeft())) h = rotateLeft(h);
+	       if (isRed(h.getLeft()) && isRed(h.getLeft().getLeft())) h = rotateRight(h);
+	       return h;   
+	       } 
+	
+	public Node rotateLeft(Node h){
+	Node x = h.getRight();
+	h.setRight(x.getLeft());
+	x.setLeft(h);
+	x.setColor(h.getColor());
+	h.setColor(RED);
+	return x;
+	}
+	
+	public Node rotateRight(Node h) {
+		Node x = h.getLeft();
+		h.setLeft(x.getRight());
+		x.setRight(h);
+		x.setColor(h.getColor());
+		h.setColor(RED);
+		return x;
+		}
+
+	public boolean isEmpty(){return (size()==0);}
 
 	@Override
 	public Key min() {
 		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) return null;
+		return (Key)min(root).getKey();
+	}
+	
+	private Node min(Node n){
+		if (n.getLeft()==null) return n;
+		return min(n.getLeft());
+	}
+	
+	private Node fixUp(Node n){
+		if(isRed(n.getRight())) n = rotateLeft(n);
+		if(isRed(n.getLeft()) && isRed(n.getLeft().getLeft())) n = rotateRight(n);
+		if(isRed(n.getLeft()) && isRed(n.getRight())) colorFlip(n);
+		
+		return n;
 	}
 
 	@Override
 	public void deleteMin() {
 		// TODO Auto-generated method stub
-		
+		 root = deleteMin(root);
+		 root.setColor(BLACK); }
+	
+	private Node deleteMin(Node h) {
+		if (h.getLeft() == null) return null;
+		if (!isRed(h.getLeft()) && !isRed(h.getLeft().getLeft()))
+			h = moveRedLeft(h);
+	    h.setLeft(deleteMin(h.getLeft()));
+	   return fixUp(h); 
 	}
-
-	@Override
-	public void delete(Key k) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	private Node moveRedLeft(Node h){
+		colorFlip(h);
+		if (isRed(h.getRight().getLeft()))
+			{h.setRight(rotateRight(h.getRight()));
+			h = rotateLeft(h);
+			colorFlip(h);
+			}
+		return h;   }
+	
+	private Node moveRedRight(Node h){
+		colorFlip(h);
+		if (isRed(h.getLeft().getLeft())){
+			h = rotateRight(h);
+			colorFlip(h);
+			}      
+		return h;
+		}
+	
+	public void delete(Key k){
+		root = delete(root, k);
+		root.setColor(BLACK);
+		}
+	
+	private Node delete(Node h, Key k){
+		if (k.compareTo((Key)h.getKey()) < 0){              
+			if (!isRed(h.getLeft()) && !isRed(h.getLeft().getLeft()))
+				h = moveRedLeft(h);
+			h.setLeft(delete(h.getLeft(), k));
+			}
+		else{
+			if (isRed(h.getLeft()))   h = rotateRight(h);               
+			if (k.compareTo((Key)h.getKey()) == 0 && (h.getRight() == null))  return null;               
+			if (!isRed(h.getRight()) && !isRed(h.getRight().getLeft())) h = moveRedRight(h);               
+			if (k.compareTo((Key)h.getKey()) == 0) {
+				h.setName(get(h.getRight(), (Key)min(h.getRight()).getKey()));
+				h.setId(min(h.getRight()).getKey());
+				h.setRight(deleteMin(h.getRight())); 
+				} 
+			else h.setRight(delete(h.getRight(), k));
+			}
+	       return fixUp(h);
+	       }
 
 	@Override
 	public String printTree() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	void colorFlip(Node h){
+		h.setColor(!h.getColor());
+		h.getLeft().setColor(h.getLeft().getColor());
+		h.getRight().setColor(h.getRight().getColor());
+		}
 
 }
