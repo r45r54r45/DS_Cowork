c43b464d9e0f001280b533368ed3fe6f0a81cd9a
Woohyun Kim <r45r54r45@naver.com>
2016-05-10 16:12:13 +0900

    io 시간 줄이기 위해 파일에 쓰는거는 맨 마지막에

--- src/WriteFile.java
+++ src/WriteFile.java
@@ -4,15 +4,25 @@ import java.io.IOException;
 public class WriteFile {
 	private String path;
 	private FileWriter fw;
+	private StringBuilder sb;
 	public WriteFile(String path){
 		this.path=path;
+		sb=new StringBuilder();
 	}
 	public void write(String str){
+		if(!str.equals("\n"))sb.append(str+" ");
+		else sb.append(str); 
+	}
+	
+	public void writeFile(){
+		
 		try{
+			System.out.println("<- File Write Start ->");
 			fw=new FileWriter(path,true);
-			if(!str.equals("\n"))fw.write(str+" ");
-			else fw.write(str);
-				fw.close();
+			fw.write(sb.toString());
+			fw.close();
+			sb=new StringBuilder();
+			System.out.println("<- File Write End ->");
 		}catch(IOException e){
 			e.printStackTrace();
 		}		 
